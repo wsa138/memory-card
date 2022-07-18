4628a370c5361b1c046019ed6ab14ba8a781c603
William Addesso <wsaddesso@gmail.com>
2022-07-17 21:07:15 -0400

    Added display grid to the CardBoard component.

--- src/components/CardBoard.js
+++ src/components/CardBoard.js
@@ -1,12 +1,8 @@
 import Card from './Card';
+import '../styles/cardBoard.css';
 
 export default function CardBoard() {
-  const cardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8].map((item) => <Card />);
+  const cardArray = [0, 1, 2, 3, 4, 5, 6, 7].map((item) => <Card />);
 
-  return (
-    <div>
-      <div>CardBoard</div>
-      {cardArray}{' '}
-    </div>
-  );
+  return <div id="cardBoard">{cardArray} </div>;
 }
