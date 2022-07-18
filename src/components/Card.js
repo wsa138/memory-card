b3aaad14e9d172720c99dd3c4f30fbc27a3ddef6
William Addesso <wsaddesso@gmail.com>
2022-07-17 21:25:54 -0400

    Passed key value and cardDisplay value to each Card component created in CardBoard.js.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,3 @@
 export default function Card(props) {
-  return <div onClick={props.increaseScore}>Card</div>;
+  return <div onClick={props.increaseScore}>{props.cardDisplay}</div>;
 }
--- src/components/CardBoard.js
+++ src/components/CardBoard.js
@@ -3,7 +3,7 @@ import '../styles/cardBoard.css';
 
 export default function CardBoard(props) {
   const cardArray = [0, 1, 2, 3, 4, 5, 6, 7].map((item) => (
-    <Card increaseScore={props.increaseScore} />
+    <Card increaseScore={props.increaseScore} key={item} cardDisplay={item} />
   ));
 
   return <div id="cardBoard">{cardArray} </div>;
