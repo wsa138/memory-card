70e28749c58156729f14c153d26d9a2d544d58a8
William Addesso <wsaddesso@gmail.com>
2022-07-31 22:05:09 -0400

    Changed image names. Imported rest of images to imageIndex.js. Added images to exported images array.

--- src/images/imageIndex.js
+++ src/images/imageIndex.js
@@ -1,6 +1,57 @@
 import arya from '../images/gotImages/arya-stark-1920.jpg';
 import bran from '../images/gotImages/bran.jpg';
+import brienne from '../images/gotImages/brienne.jpg';
+import catelyn from '../images/gotImages/catelyn.jpg';
+import cersei from '../images/gotImages/cersei.jpg';
+import daenerys from '../images/gotImages/daenerys.jpg';
+import davos from '../images/gotImages/davos.jpg';
+import hodor from '../images/gotImages/hodor.jpg';
+import jamie from '../images/gotImages/jamie.jpg';
+import jaqen from '../images/gotImages/jaqen.jpg';
+import joffery from '../images/gotImages/joffery.jpg';
+import jon from '../images/gotImages/jon-snow.jpg';
+import jorah from '../images/gotImages/jorah.jpg';
+import margarey from '../images/gotImages/margarey.jpg';
+import melisandre from '../images/gotImages/melisandre.jpg';
+import oberyn from '../images/gotImages/oberyn.jpg';
+import ramsay from '../images/gotImages/ramsay.jpg';
+import rob from '../images/gotImages/rob-stark.jpg';
+import robert from '../images/gotImages/robert.jpg';
+import sam from '../images/gotImages/sam.jpg';
+import sansa from '../images/gotImages/sansa.jpg';
+import theon from '../images/gotImages/theon.jpg';
+import tormund from '../images/gotImages/tormund.jpg';
+import tyrion from '../images/gotImages/tyrion.jpg';
+import tywin from '../images/gotImages/tywin.jpg';
+import varys from '../images/gotImages/varys.png';
 
-const images = [arya, bran];
+const images = [
+  arya,
+  bran,
+  brienne,
+  catelyn,
+  cersei,
+  daenerys,
+  davos,
+  hodor,
+  jamie,
+  jaqen,
+  joffery,
+  jon,
+  jorah,
+  margarey,
+  melisandre,
+  oberyn,
+  ramsay,
+  rob,
+  robert,
+  sam,
+  sansa,
+  theon,
+  tormund,
+  tyrion,
+  tywin,
+  varys,
+];
 
 export default images;
